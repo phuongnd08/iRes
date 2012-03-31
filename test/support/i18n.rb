482f47f4e86474389cc564c70fa34fa7d81916a9
Phuong Nguyen <phuongnd08@gmail.com>
2012-03-31 14:04:13 +0700

    Log I18n translation missing during test mode

--- test/support/i18n.rb
+++ test/support/i18n.rb
@@ -1,6 +1,16 @@
 class TestExceptionLocalizationHandler
+  def red(text)
+    "\033[31m#{text}\033[0m"
+  end
+
+  def bold(text)
+    "\033[01m#{text}\033[00m"
+  end
+
   def call(exception, locale, key, options)
-    raise "Translation for [#{locale}]#{key} does not exist"
+    msg = "Translation for [#{locale}]#{key} does not exist"
+    Rails.logger.error("[#{bold(red('ERROR'))}] #{msg}")
+    raise msg
   end
 end
 
