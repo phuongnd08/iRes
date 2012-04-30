c9c9e652a353cfd6eb87dba2fff38826b88945b2
Phuong Nguyen <phuongnd08@gmail.com>
2012-05-01 00:29:46 +0700

    Add capistrano deploy

--- Gemfile
+++ Gemfile
@@ -37,10 +37,6 @@ group :test do
   gem 'machinist', '>= 2.0.0.beta2'
   gem 'database_cleaner'
   gem 'spork', '~> 0.9.0.rc9', :require  => false
-  gem 'guard', :require => false
-  gem 'guard-spork', :require => false
-  gem 'guard-rails', :require => false
-  gem 'guard-bundler', :require => false
 end
 
 group :development, :test do
@@ -49,6 +45,19 @@ group :development, :test do
   gem 'jasminerice'
   gem 'chronic'
 end
+
+group :guard do
+  gem 'guard', :require => false
+  gem 'guard-spork', :require => false
+  gem 'guard-rails', :require => false
+  gem 'guard-bundler', :require => false
+end
+
+group :deploy do
+  gem 'capistrano'
+  gem 'rvm-capistrano'
+end
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
@@ -57,9 +66,6 @@ end
 
 # Use unicorn as the web server
 # gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+require "rvm/capistrano"
+require "bundler/capistrano"
+load "deploy/assets"
+set :application, "ires"
+set :repository,  "git@github.com:phuongnd08/iRes.git"
+set :rails_env, :production
+
+set :scm, :git
+set :branch, :master
+set :user, "deploy"
+set :use_sudo, false
+ssh_options[:forward_agent] = true
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{user}/#{application}"
+set :target_server, ENV["SERVER"]
+if target_server.nil?
+  raise "Please set target_server with SERVER=ip_or_target_server (of the target machine)"
+end
+
+
+set :rvm_ruby_string, '1.9.3-p125@ires'
+set :rvm_type, :system
+
+set :bundle_flags,    "--deployment"
+set :bundle_without,  [:development, :test, :deploy]
+
+role :app, target_server
+role :db, target_server, :primary => true                          # Your HTTP target_server, Apache/etc
+role :web, target_server
+
+namespace :deploy do
+  task :start, :roles => [:web, :app] do
+  end
+
+  task :stop, :roles => [:web, :app] do
+  end
+
+  task :restart, :roles => [:web, :app] do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
