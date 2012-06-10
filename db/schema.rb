31bf2745caed4c05cfad542b015af22fd3d84d63
Phuong Nguyen <phuongnd08@gmail.com>
2012-06-10 16:38:25 +0700

    Update schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515045947) do
+ActiveRecord::Schema.define(:version => 20120603161322) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20120515045947) do
     t.datetime "updated_at",                    :null => false
     t.integer  "price"
     t.boolean  "ready",      :default => false
+    t.string   "notes"
   end
 
   create_table "orders", :force => true do |t|
